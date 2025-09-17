ee72515a3f5aa985c4e44b796778c81ebd4f9e17
kincicd252867 <168291013+kincicd252867@users.noreply.github.com>
2025-09-17 16:32:46 +0800

    Update __init__.py

--- src/my_project/__init__.py
+++ src/my_project/__init__.py
@@ -1,1 +1,24 @@
+"""
+My Databricks Project
 
+A package containing data transformation utilities and helpers for Databricks ETL workflows.
+"""
+
+__version__ = "1.0.0"
+__author__ = "Data Engineering Team"
+__description__ = "Databricks ETL transformation utilities"
+
+# Import key functions to make them available at package level
+from .data_transforms import (
+    clean_column_names,
+    enforce_schema,
+    add_ingestion_metadata
+)
+
+# Define what gets imported with "from my_project import *"
+__all__ = [
+    'clean_column_names',
+    'enforce_schema',
+    'add_ingestion_metadata',
+    'utils'  # This makes the utils subpackage available
+]
